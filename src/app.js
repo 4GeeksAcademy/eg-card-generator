e00a8f62e5cb551bb9ac721cbc3585637c4c8e61
gomeseric <gomesericson.a@gmail.com>
2024-06-14 23:56:02 +0000

    test

--- src/app.js
+++ src/app.js
@@ -5,7 +5,22 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function randomItem(arr) {
+  const index = randomIndex(arr);
+  return arr[index];
+}
+
+function createCard () {
+  const cardHTML = `<div class="bg-slate-100 w-[250px] h-[300px]"> .K. </div>`;
+
+  const d = document.createElement("div");
+  d.innerHTML = cardHTML;
+  document.body.appendChild(d);
+}
+
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+createCard();
