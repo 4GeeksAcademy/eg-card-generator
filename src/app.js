b24f957ca5192afceb831efa15d5339b2aa8899e
gomeseric <gomesericson.a@gmail.com>
2024-06-15 00:28:27 +0000

    test 3

--- src/app.js
+++ src/app.js
@@ -5,13 +5,31 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function randomIndex(arr) {
+  return Math.floor(Math.random() * arr.length);
+}
+
 function randomItem(arr) {
   const index = randomIndex(arr);
   return arr[index];
 }
 
-function createCard () {
-  const cardHTML = `<div class="bg-slate-100 w-[250px] h-[350px]"> .K. </div>`;
+function createCard() {
+  const cardValues = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  const cardSuits = ["♠", "♥", "♣", "♦"];
+
+  const value = randomItem(cardValues);
+  const suit = randomItem(cardSuits);
+
+  const suitColor =
+    suit === "♠" || suit === "♣" ? "text-black" : "text-red-400":
+
+  const cardHTML = `<div class="my-card w-[250px] h-[350px] cursor-pointer bg-white rounded-xl flex justify-center items-center relative">
+      <div class="z-10 text-7xl text-black">${value}</div>
+      <div class="text-black text-6xl absolute top-2 left-2">${suit}</div>
+    
+      <div class="text-black text-6xl absolute bottom-2 right-2 rotate-180">${suit}</div>
+    </div>`;
 
   const d = document.createElement("div");
   d.innerHTML = cardHTML;
@@ -24,3 +42,4 @@ window.onload = function() {
 };
 
 createCard();
+};
